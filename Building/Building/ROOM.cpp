1947f66c394984e758f73c277ee71490bfd4607a
william·lone <donwilliamlone@gmail.com>
2015-05-02 20:09:26 +0800

    更改了项目的实现方式
    使用MFC的 ODBC类，而不是创建项目时赋予的SET类

---
+++ Building/Building/ROOM.cpp
@@ -0,0 +1,124 @@
+// ROOM.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "Building.h"
+#include "BuildingSet.h"
+#include "ROOM.h"
+#include "afxdialogex.h"
+
+
+// ROOM 对话框
+
+IMPLEMENT_DYNAMIC(ROOM, CDialogEx)
+
+ROOM::ROOM(CWnd* pParent /*=NULL*/)
+	: CDialogEx(ROOM::IDD, pParent)
+{			//控件的初始化不要写在这里，当这个对象被创立时，窗口还没有创建，何谈控件？
+}
+
+ROOM::~ROOM()
+{
+}
+
+void ROOM::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, List);
+}
+
+BEGIN_MESSAGE_MAP(ROOM, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &ROOM::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &ROOM::OnBnClickedButton2)
+	ON_BN_CLICKED(IDC_BUTTON3, &ROOM::OnBnClickedButton3)
+END_MESSAGE_MAP()
+
+// ROOM 消息处理程序
+
+BOOL ROOM::OnInitDialog()				//重写的窗口初始化函数，在这里可以写一些控件的初始化
+{
+	CDialogEx::OnInitDialog();
+	int index;
+	// TODO:  在此添加额外的初始化
+	//窗口控件的初始化
+	List.ModifyStyle(0, LVS_REPORT | LVS_SINGLESEL | LVS_SHOWSELALWAYS);
+	List.SetTextColor(RGB(0, 0, 0));
+	List.SetBkColor(RGB(255, 255, 255));
+	List.InsertColumn(0, _T("RoomID"), LVCFMT_CENTER, 60, 0);
+	List.InsertColumn(1, _T("NumberOfSeat"), LVCFMT_CENTER, 100, 0);
+	List.InsertColumn(2, _T("NumberOfComputer"), LVCFMT_CENTER, 140, 0);
+	List.InsertColumn(3, _T("NumberOfWhitebash"), LVCFMT_CENTER, 140, 0);
+	List.InsertColumn(4, _T("NumberOfProject"), LVCFMT_CENTER, 140, 0);
+	//变量初始化
+	RoomID = "";
+	NumberOfSeat = "";
+	NumberOfComputer = "";
+	NumberOfWhitebash = "";
+	NumberOfProject = "";
+	//数据库连接初始化
+	CString DSNname = _T("DSN=BuildingManager");
+	Record = new CRecordset(&DB);
+	if (DB.OpenEx(DSNname, 0)){
+		if (Record->Open(CRecordset::dynaset, _T("select * from detailofroom7"), CRecordset::none)){
+			MessageBox(_T("成功的与数据库建立连接"));
+		}
+		else{
+			MessageBox(_T("没有检索到表"));
+		}
+	}
+	else{
+		MessageBox(_T("数据库连接失败"));
+	}
+	
+
+	
+
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// 异常:  OCX 属性页应返回 FALSE
+}
+
+void ROOM::OnBnClickedButton1()
+{
+	UpdateData();			//从控件更新数据到变量
+	
+	int index;			//创建一个列表控件的行数标记变量
+	//从记录集复制数据到本地变量
+	Record->MoveFirst();				//确保记录集指向第一个记录
+
+	for (index = 0; !(Record->IsEOF()); index++){			//不断循环，  当记录集有数据的时候，isEOF方法为0，所以在前面加上！，以保证循环
+		Record->GetFieldValue((short)0, RoomID);			//从记录集读取数据到变量
+		Record->GetFieldValue((short)1, NumberOfSeat);
+		Record->GetFieldValue((short)2, NumberOfComputer);
+		Record->GetFieldValue((short)3, NumberOfWhitebash);
+		Record->GetFieldValue((short)4, NumberOfProject);
+		Record->MoveNext();							//记录集移动到下一条
+		//定义行
+		index = List.GetItemCount();//对标题进行初始化
+		LV_ITEM lvitem;
+		lvitem.iItem = index;
+		lvitem.mask = LVFIF_TEXT;
+		lvitem.iSubItem = 0;
+		lvitem.pszText = (LPWSTR)(char *)(LPCTSTR)RoomID;
+		//插入一行
+		List.InsertItem(&lvitem);				//插入一行，
+		List.SetItemText(index, 0, RoomID);				//在这个坐标的格子上，插入变量
+		List.SetItemText(index, 1, NumberOfSeat);
+		List.SetItemText(index, 2, NumberOfComputer);
+		List.SetItemText(index, 3, NumberOfWhitebash);
+		List.SetItemText(index, 4, NumberOfProject);
+	}
+
+	UpdateData(false);				//更新数据到控件
+}
+
+void ROOM::OnBnClickedButton2()
+{
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+void ROOM::OnBnClickedButton3()
+{
+	// TODO:  在此添加控件通知处理程序代码
+}
--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# EducationManager
+﻿# EducationManager
 一个教务管理系统
 这个软件用Visual Studio2012作为工具，MFC框架开发的，目的：方便多媒体管理人员的管理工作，
 
-
-Access数据库文件位置：Manager\Manager\Database\...
-
-
-Access数据库日志文件位置：Manager\Manager\Database\log.txt
+数据库文件在DataBase文件夹中，
+用这个数据库文件创建一个数据源名为“ Building ”，
+这样，该软件就能从数据源内读取数据了，
